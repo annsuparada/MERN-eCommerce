7da1da294e8fcbdfb8e0fe82f7e5218742991a29
Ann <ann.suparada@gmail.com>
2022-07-19 14:28:49 +0700

    connect Mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
+import { connect } from 'http2';
 
 dotenv.config();
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
